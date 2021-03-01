9782346998b8cb275733ecd8908bf9fe704c127e
Sezer YILDIRIM <sezer.yildirim@live.com>
2021-03-01 13:19:12 +0300

    GeneralMessages Multiple Language Support (EN, TR, DE, ES, RU)

--- SYCore/Utilities/Messages/GeneralMessages.cs
+++ SYCore/Utilities/Messages/GeneralMessages.cs
@@ -10,35 +10,151 @@ namespace SYCore.Utilities.Messages
     {
         public static string ERROR_MESSAGE = "SORRY, AN ERROR HAS OCCURED! DETAILS: ";
 
-        public static string NOT_FOUND(string name)
+        public static string NOT_FOUND(string name, string langCode = null)
         {
+            if (langCode.Equals("TR", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " BULUNAMADI!";
+            }
+            else if (langCode.Equals("DE", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NICHT GEFUNDEN!";
+            }
+            else if (langCode.Equals("ES", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NO ENCONTRADO!";
+            }
+            else if (langCode.Equals("RU", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " НЕ НАЙДЕН!";
+            }
+
             return name + " NOT FOUND!";
         }
 
-        public static string ADDED(string name)
+        public static string ADDED(string name, string langCode = null)
         {
-            return name + " ADDED SUCCESFULLY";
+            if (langCode.Equals("TR", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " BASARIYLA EKLENDI!";
+            }
+            else if (langCode.Equals("DE", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " ERFOLGREICH HINZUGEFUGT!";
+            }
+            else if (langCode.Equals("ES", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " AGREGADO EXITOSAMENTE!";
+            }
+            else if (langCode.Equals("RU", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " УСПЕШНО ДОБАВЛЕН!";
+            }
+
+            return name + " ADDED SUCCESSFULLY";
         }
-        public static string NOT_ADDED(string name)
+        public static string NOT_ADDED(string name, string langCode = null)
         {
+            if (langCode.Equals("TR", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " EKLENEMEDI!";
+            }
+            else if (langCode.Equals("DE", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NICHT HINZUGEFUGT!";
+            }
+            else if (langCode.Equals("ES", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NO AÑADIDO!";
+            }
+            else if (langCode.Equals("RU", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " НЕ ДОБАВЛЕН!";
+            }
+
             return name + " NOT ADDED!";
         }
 
-        public static string DELETED(string name)
+        public static string DELETED(string name, string langCode = null)
         {
-            return name + " DELETED SUCCESFULLY";
+            if (langCode.Equals("TR", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " BASARIYLA SILINDI!";
+            }
+            else if (langCode.Equals("DE", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " ERFOLGREICH GELÖSCHT!";
+            }
+            else if (langCode.Equals("ES", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " BORRADO EXITOSAMENTE!";
+            }
+            else if (langCode.Equals("RU", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " УДАЛЕНО УСПЕШНО!";
+            }
+            return name + " DELETED SUCCESSFULLY";
         }
-        public static string NOT_DELETED(string name)
+        public static string NOT_DELETED(string name, string langCode = null)
         {
+            if (langCode.Equals("TR", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " SILINEMEDI!";
+            }
+            else if (langCode.Equals("DE", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NICHT GELÖSCHT!";
+            }
+            else if (langCode.Equals("ES", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NO ELIMINADA/NO ELIMINADO!";
+            }
+            else if (langCode.Equals("RU", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " НЕ УДАЛЕНО!";
+            }
             return name + " NOT DELETED!";
         }
 
-        public static string UPDATED(string name)
+        public static string UPDATED(string name, string langCode = null)
         {
-            return name + " UPDATED SUCCESFULLY";
+            if (langCode.Equals("TR", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " BASARIYLA GUNCELLENDI!";
+            }
+            else if (langCode.Equals("DE", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " ERFOLGREICH GEUPDATED!";
+            }
+            else if (langCode.Equals("ES", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " ACTUALIZADO CON ÉXITO!";
+            }
+            else if (langCode.Equals("RU", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " УСПЕШНО ОБНОВЛЕНО!";
+            }
+            return name + " UPDATED SUCCESSFULLY";
         }
-        public static string NOT_UPDATED(string name)
+        public static string NOT_UPDATED(string name, string langCode = null)
         {
+            if (langCode.Equals("TR", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " GUNCELLENEMEDI!";
+            }
+            else if (langCode.Equals("DE", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NICHT AKTUALISIERT!";
+            }
+            else if (langCode.Equals("ES", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " NO ACTUALIZADA/NO ACTUALIZADO!";
+            }
+            else if (langCode.Equals("RU", StringComparison.OrdinalIgnoreCase))
+            {
+                return name + " НЕ ОБНОВЛЯЮТСЯ!";
+            }
+
             return name + " NOT UPDATED!";
         }
     }
